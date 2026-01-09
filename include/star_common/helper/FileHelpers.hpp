c0850f807dfdaf989c54387b19667bfdea7b3b1b
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-01-09 12:05:35 -0500

    Add file helper

---
+++ include/star_common/helper/FileHelpers.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <filesystem>
+#include <limits.h>
+#include <unistd.h>
+
+namespace star::common
+{
+std::filesystem::path GetExecutablePath()
+{
+#if defined(_WIN32)
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+    return buffer;
+
+#elif defined(__linux__)
+    char buffer[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer));
+    return std::filesystem::path(std::string(buffer, len));
+
+#elif defined(__APPLE__)
+    char buffer[PATH_MAX];
+    uint32_t size = sizeof(buffer);
+    _NSGetExecutablePath(buffer, &size);
+    return std::filesystem::canonical(buffer);
+
+#else
+#error Unsupported platform
+#endif
+}
+} // namespace star::common
