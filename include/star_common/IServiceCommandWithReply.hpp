1eaa2029a8cce7244dbcc170e28414e0146ffa4b
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-02-08 16:37:35 -0500

    Add constructor for IServiceCommandWithReply

--- include/star_common/IServiceCommandWithReply.hpp
+++ include/star_common/IServiceCommandWithReply.hpp
@@ -9,6 +9,10 @@ namespace star::common
 template <typename T> class IServiceCommandWithReply : public IServiceCommand
 {
   public:
+    IServiceCommandWithReply() = default;
+    explicit IServiceCommandWithReply(uint16_t type) : IServiceCommand(std::move(type))
+    {
+    }
     common::ServiceReply<T> &getReply()
     {
         return result;
